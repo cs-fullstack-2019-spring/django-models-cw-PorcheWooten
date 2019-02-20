504e16e6958095647e5184752691bf1f7aa6fb77
PorcheWooten <porchewooten@yahoo.com>
2019-02-20 14:44:34 -0600

    graded

--- classworkProject/classworkApp/admin.py
+++ classworkProject/classworkApp/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Dog
 from .models import Account
-
+# register dog model(class)
 admin.site.register(Dog)
+# register account model
 admin.site.register(Account)
